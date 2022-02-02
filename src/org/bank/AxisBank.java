e930e2e3ccf03e6e42a2f14062e7ef694bf4dc1f
nirmalprasanth <nirmalprasanth96@gmail.com>
2022-02-02 09:29:16 +0530

    modified

--- src/org/bank/AxisBank.java
+++ src/org/bank/AxisBank.java
@@ -5,11 +5,10 @@ public class AxisBank extends BankINFO {
 		System.out.println("Amount deposited in 2021:" + Amount);
 		super.deposit(75000);
 	}
-	private void bb() {
-		System.out.println("bank");
+	private void ba() {
+		System.out.println("invalid");
 
 	}
-
 	public static void main(String[] args) {
 		AxisBank A = new AxisBank();
 		A.deposit(150000);
