95dab0c33a11b92da7586c900fca9789ce4809a4
nirmalprasanth <nirmalprasanth96@gmail.com>
2022-02-02 08:40:24 +0530

    gowshik1

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,14 @@
+package org.bank;
+
+public class AxisBank extends BankINFO {
+	public void deposit(int Amount) {
+		System.out.println("Amount deposited in 2021:" + Amount);
+		super.deposit(75000);
+	}
+
+	public static void main(String[] args) {
+		AxisBank A = new AxisBank();
+		A.deposit(150000);
+	}
+
+}
---
+++ src/org/bank/BankINFO.java
@@ -0,0 +1,9 @@
+package org.bank;
+
+public class BankINFO {
+public void deposit(int Amount) {
+	System.out.println("Amount deposited in 2020:"+Amount);
+	
+}
+
+}
